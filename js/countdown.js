58de036c309796e22a3aab356994f8fe22d7ad77
caryoscelus <caryoscelus@gmx.com>
2016-03-28 19:26:53 +0300

    simple countdown js

---
+++ js/countdown.js
@@ -0,0 +1,28 @@
+function countdown(d) {
+    var target = document.getElementById('countdown');
+    var diff, dd, dh, dm, ds;
+    function full_update() {
+        diff = (new Date(Date.parse(d))-new Date())/1000;
+        update_target();
+    }
+    function update() {
+        diff -= 1;
+        update_target();
+    }
+    function update_target() {
+        var diff1, diff2;
+        dd = parseInt(diff / (60*60*24)),
+        diff1 = diff % (60*60*24),
+        dh = parseInt(diff1 / (60*60)),
+        diff2 = diff1 % (60*60),
+        dm = parseInt(diff2 / 60),
+        ds = parseInt(diff2 % 60);
+        target.innerHTML = dd+'/'+a0(dh)+':'+a0(dm)+':'+a0(ds);
+    }
+    function a0(n) {
+        if (n < 10) return '0'+n;
+        return n;
+    }
+    full_update();
+    setInterval(update, 1000);
+}
