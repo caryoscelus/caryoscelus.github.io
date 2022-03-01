05c11775694f6c8bef68524a2c8bcf179738c008
caryoscelus <caryoscelus@gmx.com>
2022-03-01 10:15:35 +0000

    riza-0-demo-x

---
+++ riza-0-demo/jAgda.Agda.Primitive.js
@@ -0,0 +1,12 @@
+define(['agda-rts' ],
+
+function(agdaRTS ) {
+
+var exports = {};
+
+exports["Level"] = undefined;
+exports["lzero"] = undefined;
+exports["lsuc"] = undefined;
+exports["_⊔_"] = undefined;
+
+return exports; });
